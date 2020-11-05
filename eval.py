41ea33928f724545ba3350980bb1d53669901cca
Zheng Tang <oscartang1120@gmail.com>
2020-11-04 23:40:37 -0700

    Update eval.py

--- eval.py
+++ eval.py
@@ -82,6 +82,7 @@ for batch in eval_batch.data_r:
             else:
                 candidate.append(vocab.id2rule[int(r)])
         if count in randomlist:
+            print (sentence_bleu(reference, candidate))
             print (id2label[batch.rel.tolist()[i]], id2label[preds[i]])
             print ([vocab.id2word[w] for w in batch.words.view(batch_size, -1)[i].tolist() if w != 0])
             print (reference[0])
