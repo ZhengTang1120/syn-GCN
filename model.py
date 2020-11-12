614852e174cbf503777a8e8fcc767c2e3feed8f6
Zheng Tang <oscartang1120@gmail.com>
2020-11-12 06:05:11 -0700

    classifier only

--- model.py
+++ model.py
@@ -56,24 +56,24 @@ class RelationModel(object):
         if self.opt.get('pooling_l2', 0) > 0:
             loss += self.opt['pooling_l2'] * (pooling_output ** 2).sum(1).mean()
         #DECODER PART
-        rules = rules.view(batch_size, -1)
-        masks = inputs[1]
-        max_len = rules.size(1)
-        rules = rules.transpose(1,0)
-        output = Variable(torch.LongTensor([constant.SOS_ID] * batch_size)) # sos
-        output = output.cuda() if self.opt['cuda'] else output
-        loss_d = 0
-        h0 = hidden.view(self.opt['num_layers'], batch_size, -1)
-        c0 = hidden.view(self.opt['num_layers'], batch_size, -1)
-        decoder_hidden = (h0, c0)
-        for t in range(1, max_len):
-            output, decoder_hidden, attn_weights = self.decoder(
-                    output, masks, decoder_hidden, encoder_outputs)
-            loss_d += self.criterion_d(output, rules[t])
-            output = rules.data[t]
-            if self.opt['cuda']:
-                output = output.cuda()
-        loss += loss_d/max_len if max_len!=0 else loss_d
+        # rules = rules.view(batch_size, -1)
+        # masks = inputs[1]
+        # max_len = rules.size(1)
+        # rules = rules.transpose(1,0)
+        # output = Variable(torch.LongTensor([constant.SOS_ID] * batch_size)) # sos
+        # output = output.cuda() if self.opt['cuda'] else output
+        # loss_d = 0
+        # h0 = hidden.view(self.opt['num_layers'], batch_size, -1)
+        # c0 = hidden.view(self.opt['num_layers'], batch_size, -1)
+        # decoder_hidden = (h0, c0)
+        # for t in range(1, max_len):
+        #     output, decoder_hidden, attn_weights = self.decoder(
+        #             output, masks, decoder_hidden, encoder_outputs)
+        #     loss_d += self.criterion_d(output, rules[t])
+        #     output = rules.data[t]
+        #     if self.opt['cuda']:
+        #         output = output.cuda()
+        # loss += loss_d/max_len if max_len!=0 else loss_d
 
         # backward
         loss.backward()
